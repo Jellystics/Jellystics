ed1bbb1da802fa18318366ea968756837232848f
cedev <cedevserver@gmail.com>
2026-06-15 12:45:43 +0200

    init README

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Jellystat";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ 
+        "x86_64-linux" 
+        "aarch64-darwin"
+        ];
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_24
+              pnpm
+	      go-task
+            ];
+          };
+        }
+      );
+    };
+}
